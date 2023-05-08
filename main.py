894f8418a499d936278dee4c3d5bf7c8f755f2e7
PatrickStyle66 <jphamega@hotmail.com>
2023-05-08 19:07:49 -0300

    feat(Lexical):Added lexical Analyzer

--- main.py
+++ main.py
@@ -7,8 +7,8 @@ reserved = {
     'begin':'BEGIN',
     'end':'END',
     'program':'PROGRAM',
-    'pilha of integer':'PILHA OF INTEGER',
-    'pilha of real':'PILHA OF REAL',
+    'pilha_of_integer':'PILHAINTEGER',
+    'pilha_of_real':'PILHAREAL',
     'var':'VAR',
     'function':'FUNCTION',
     'read':'READ',
@@ -21,3 +21,89 @@ reserved = {
     'to':'TO',
     'do':'DO',
 }
+tokens = [
+    'ID',
+    'NUMBER',
+    'RNUMBER',
+    'PLUS',
+    'MINUS',
+    'TIMES',
+    'DIVIDE',
+    'DIVIDER',
+    'LPAREN',
+    'RPAREN',
+    'GREAT',
+    'LESS',
+    'GREATEQ',
+    'LESSEQ',
+    'EQUALS',
+    'DIFFERENT',
+    'ATTR',
+    'COMMA',
+    'DOTCOMMA',
+    'DOT',
+    'TWODOT',
+    'HASHTAG'
+]
+
+tokens = tokens + list(reserved.values())
+
+t_PLUS   = r'\+'
+t_MINUS  = r'-'
+t_TIMES  = r'\*'
+t_DIVIDE = r'//'
+t_DIVIDER = r'/'
+t_LPAREN = r'\('
+t_RPAREN = r'\)'
+t_GREAT = r'>'
+t_LESS = r'<'
+t_GREATEQ = r'>='
+t_LESSEQ = r'<='
+t_EQUALS = r'='
+t_DIFFERENT = r'<>'
+t_ATTR = r':='
+t_COMMA = r','
+t_DOTCOMMA = r';'
+t_DOT = r'\.'
+t_TWODOT = r':'
+t_HASHTAG = r'\#'
+
+errors =[]
+
+def t_RNUMBER(t):
+    r'[0-9]+\.?[0-9]+'
+    t.value = float(t.value)
+    return t
+def t_NUMBER(t):
+    r'\d+'
+    t.value = int(t.value)
+    return t
+
+def t_newline(t):
+    r'\n+'
+    t.lexer.lineno += len(t.value)
+
+t_ignore  = ' \t'
+
+def t_error(t):
+    errors.append(f'({t.value[0]},lin: {t.lineno})')
+    t.lexer.skip(1)
+
+def t_ID(t):
+    r'[a-zA-Z_][a-zA-Z_0-9]*'
+    t.type = reserved.get(t.value,'ID')
+    return t
+
+lexer = lex.lex()
+
+file = open('test.txt','r')
+data = file.read()
+lexer.input(data)
+
+while True:
+    tok = lexer.token()
+    if not tok:
+        break
+    print(f'({tok.type}, {tok.value}) lin: {tok.lineno}')
+if errors:
+    print(f'Erros léxicos:{errors}')
